a9f6f6f3b4353050870cf7ac374fc521f497c604
Gul Jamal Zim <guljamalzim@gmail.com>
2021-12-15 16:32:09 +0600

    Add tailwind modifications

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,22 @@ module.exports = {
             fontFamily: {
                 sans: ['Nunito', ...defaultTheme.fontFamily.sans],
             },
+            colors: {
+                gray: {
+                    '100': '#f5f5f5',
+                    '200': '#eeeeee',
+                    '300': '#e0e0e0',
+                    '400': '#bdbdbd',
+                    '500': '#9e9e9e',
+                    '600': '#757575',
+                    '700': '#616161',
+                    '800': '#424242',
+                    '900': '#212121',
+                }
+            },
+            spacing: {
+                'xs': '2px'
+            }
         },
     },
 
