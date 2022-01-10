63f8005379d443c5a0dfd91538658f846846f4c0
Gul Jamal Zim <guljamalzim@gmail.com>
2022-01-10 13:53:08 +0600

    Use blade application-logo component in header partial file

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -1,7 +1,5 @@
 <header class="flex justify-between items-center my-2 mb-4">
-    <p class="font-bold md:text-3xl lg:text-5xl text-gray-500">
-        <a href="{{ route('home') }}">{{ config('app.name') }}</a>
-    </p>
+    <a href="/"><x-application-logo class="w-20 h-20 fill-current text-gray-500" /></a>
 
     @include('layouts.partials.navigation')
 </header>
