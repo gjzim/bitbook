7fd96d02d657b341d9b61405c04315289141ff53
Gul Jamal Zim <guljamalzim@gmail.com>
2022-01-11 12:24:57 +0600

    Modify blade components' styles

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,3 +1,3 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150']) }}>
+<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent text-sm text-white tracking-widest bg-blue-600 hover:bg-blue-500 active:bg-blue-900 focus:outline-none focus:border-blue-900 focus:ring focus:ring-blue-200 focus:ring-opacity-50 disabled:opacity-25 transition ease-in-out duration-150']) }}>
     {{ $slot }}
 </button>
--- resources/views/components/select.blade.php
+++ resources/views/components/select.blade.php
@@ -5,7 +5,7 @@
 
 <select
     {{ $disabled ? 'disabled' : '' }}
-    {!! $attributes->merge(['class' => 'block mt-1 w-full rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50']) !!}
+    {!! $attributes->merge(['class' => 'block mt-1 w-full shadow-sm border-gray-300 focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50']) !!}
 >
     <option value=""></option>
     @foreach($options as $value => $text)
