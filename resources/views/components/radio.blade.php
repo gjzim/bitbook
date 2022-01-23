04433efccfd455ded60562e3c64ccf8074ac23fb
Gul Jamal Zim <guljamalzim@gmail.com>
2022-01-23 09:16:26 +0600

    Add textarea & radio blade components

---
+++ resources/views/components/radio.blade.php
@@ -0,0 +1,4 @@
+<div class="inline-flex items-center">
+    <input type="radio" {!! $attributes->merge(['class' => 'shadow-sm border-gray-300']) !!}>
+    <span class="ml-2 mr-4">{{$slot}}</span>
+</div>
