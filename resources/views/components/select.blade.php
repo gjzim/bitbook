0e5c2e3812744e3913f7a9989d9a7043dc25aeec
Gul Jamal Zim <guljamalzim@gmail.com>
2022-01-23 09:17:37 +0600

    Update select blade component to select a default option

--- resources/views/components/select.blade.php
+++ resources/views/components/select.blade.php
@@ -1,14 +1,15 @@
 @props([
 'options' => [],
-'disabled' => false
+'disabled' => false,
+'selected' => '',
 ])
 
 <select
     {{ $disabled ? 'disabled' : '' }}
     {!! $attributes->merge(['class' => 'block mt-1 w-full shadow-sm border-gray-300 focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50']) !!}
 >
-    <option value=""></option>
+    <option value="" {{$selected == '' ? 'selected' : ''}}></option>
     @foreach($options as $value => $text)
-        <option value="{{$value}}" {{ old($attributes['name']) == $value ? 'checked' : '' }}>{{__($text)}}</option>
+        <option value="{{$value}}" {{$selected == $value ? 'selected' : ''}}>{{__($text)}}</option>
     @endforeach
 </select>
