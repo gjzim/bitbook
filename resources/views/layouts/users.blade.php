5f514e312aacd0dfcf8f718b1ed068d2ba5d5267
Gul Jamal Zim <guljamalzim@gmail.com>
2022-02-11 11:56:35 +0600

    Create user layout blade file

---
+++ resources/views/layouts/users.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.master')
+
+@section('sidebar-menu')
+    <x-sidebar-menu>
+        <x-sidebar-menu-item to="{{ route('users.show', ['user' => $user]) }}" fa-class="fa-user">About
+        </x-sidebar-menu-item>
+        <x-sidebar-menu-item to="{{ route('users.show.posts', ['user' => $user]) }}" fa-class="fa-th-list">Posts
+        </x-sidebar-menu-item>
+        @if (auth()->user()->can('update', $user))
+            <x-sidebar-menu-item to="{{ route('users.edit', ['user' => $user]) }}" fa-class="fa-edit">Edit Profile
+            </x-sidebar-menu-item>
+            <x-sidebar-menu-item to="{{ route('users.change.password', ['user' => $user]) }}" fa-class="fa-wrench">
+                Change Password
+            </x-sidebar-menu-item>
+            <x-sidebar-menu-item to="{{ route('users.delete.confirm', ['user' => $user]) }}" fa-class="fa-trash">
+                Delete Profile
+            </x-sidebar-menu-item>
+        @endif
+    </x-sidebar-menu>
+@endsection
+
+@section('content')
+    @yield('content')
+@endsection
