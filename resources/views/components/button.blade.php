27264d52a059458c3607eb51cd26cd1126c8e86b
Gul Jamal Zim <guljamalzim@gmail.com>
2022-02-11 11:53:36 +0600

    Add variant support for button blade component

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,3 +1,17 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent text-sm text-white tracking-widest bg-blue-600 hover:bg-blue-500 active:bg-blue-900 focus:outline-none focus:border-blue-900 focus:ring focus:ring-blue-200 focus:ring-opacity-50 disabled:opacity-25 transition ease-in-out duration-150']) }}>
+@props([
+'variant' => '',
+])
+
+@php
+    $classes = "inline-flex items-center px-4 py-2 border border-transparent text-white focus:ring focus:ring-opacity-50 disabled:opacity-25 transition ease-in-out duration-150 ";
+
+    if ($variant === 'error') {
+        $classes .= "bg-red-600 hover:bg-red-500 active:bg-red-900 focus:border-red-900 focus:ring-red-200";
+    } else {
+        $classes .= "bg-blue-600 hover:bg-blue-500 active:bg-blue-900 focus:border-blue-900 focus:ring-blue-200";
+    }
+@endphp
+
+<button {{ $attributes->merge(['type' => 'submit', 'class' => $classes]) }}>
     {{ $slot }}
 </button>
