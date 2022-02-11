ff47bfbd40a6fa369d1abd8f621e648f7ab0ccd5
Gul Jamal Zim <guljamalzim@gmail.com>
2022-02-11 11:54:45 +0600

    Update sidebar-menu-item blade component to show selected link

--- resources/views/components/sidebar-menu-item.blade.php
+++ resources/views/components/sidebar-menu-item.blade.php
@@ -4,7 +4,10 @@
 'selected' => false
 ])
 
-@php($classes = 'block px-5 py-1 ' . ($selected ? 'bg-blue-500 text-white pointer-events-none' : 'text-blue-500'))
+@php
+    $selected = request()->url() === $to;
+    $classes = 'block px-5 py-1 ' . ($selected ? 'bg-blue-500 text-white pointer-events-none' : 'text-blue-500');
+@endphp
 
 <li>
     <a href="{{$to}}" {{ $attributes->merge(['class' => $classes]) }}>
