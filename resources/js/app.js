e1f9ffe06f40b45e70da33b2dbb2d9d391479348
Gul Jamal Zim <guljamalzim@gmail.com>
2022-02-23 11:25:23 +0600

    Install @alpine/intersect plugin

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,7 +2,9 @@ require('./bootstrap');
 require('@fortawesome/fontawesome-free/js/all.min.js');
 
 import Alpine from 'alpinejs';
+import intersect from '@alpinejs/intersect'
 
+Alpine.plugin(intersect)
 window.Alpine = Alpine;
 
 Alpine.start();
