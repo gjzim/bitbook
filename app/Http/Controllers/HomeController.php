16e46d26d610c320517afd40286164cc78994839
Gul Jamal Zim <guljamalzim@gmail.com>
2022-02-26 14:50:41 +0600

    Add home/posts route and controller action method

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Resources\PostResource;
+use App\Models\Post;
 
 class HomeController extends Controller
 {
@@ -11,8 +12,19 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('layouts.home', [
-            'posts' => []
-        ]);
+        return view('layouts.home');
+    }
+
+    /**
+     * Return home feed posts of the currently logged in user.
+     */
+    public function postsIndex()
+    {
+        $posts = Post::with(['author', 'author.media', 'media'])
+            ->whereIn('user_id', [auth()->user()->id, ...auth()->user()->getFriendsIds()])
+            ->orderBy('created_at', 'desc')
+            ->paginate(10);
+
+        return PostResource::collection($posts);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [HomeController::class, 'index'])
     ->name('home')->middleware('auth');
 
+Route::get('/home/posts', [HomeController::class, 'postsIndex'])
+    ->name('home.posts.index')->middleware(['auth']);
+
 Route::get('/users/{user}', [UserController::class, 'show'])
     ->name('users.show')->middleware(['auth']);
 
