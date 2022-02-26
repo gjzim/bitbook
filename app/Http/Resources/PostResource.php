3d71c5a204a8bbf08b7ce32ab45011635916ca24
Gul Jamal Zim <guljamalzim@gmail.com>
2022-02-26 14:49:17 +0600

    Add Post resource and collection classes

---
+++ app/Http/Resources/PostResource.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PostResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'url' => route('posts.show', ['post' => $this]),
+            'content' => $this->content,
+            'author' => new UserResource($this->whenLoaded('author')),
+            'image' => $this->when($this->relationLoaded('media') && $this->getMedia('images')->isNotEmpty(), function () {
+                return [
+                    'full' => $this->getMedia('images')->last()->getUrl(),
+                    'large' => $this->getMedia('images')->last()->getUrl('large'),
+                ];
+            }),
+            'visibility' => $this->visibility,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
