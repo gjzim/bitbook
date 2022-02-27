b6f9bb787652a5021076849c4f30a39bcdc4d914
Gul Jamal Zim <guljamalzim@gmail.com>
2022-02-27 14:08:34 +0600

    Add like model, migration, and controller classes

---
+++ app/Http/Controllers/LikeController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Like;
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class LikeController extends Controller
+{
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Models\Post $post
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Post $post)
+    {
+        Like::create([
+            'post_id' => $post->id,
+            'user_id' => auth()->user()->id,
+        ]);
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Successfully liked the post',
+            'data' => []
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Like  $like
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Post $post)
+    {
+        Like::where([
+            'post_id' => $post->id,
+            'user_id' => auth()->user()->id,
+        ])->delete();
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Successfully unliked the post',
+            'data' => []
+        ]);
+    }
+}
