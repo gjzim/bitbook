9a7d190a4a1fd39fd60127a4c7176e66c2a9c755
Gul Jamal Zim <guljamalzim@gmail.com>
2022-03-01 11:40:04 +0600

    Randomly generate password hashes in UserFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 class UserFactory extends Factory
@@ -24,7 +25,7 @@ class UserFactory extends Factory
             'birthdate' => $this->faker->dateTime(),
             'country' => $this->faker->countryCode(),
             'about' => $this->faker->paragraph(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => Hash::make(Str::random(10)),
             'remember_token' => Str::random(10),
         ];
     }
