0f2d28ebe8a40299ecb8faaecec8423f571dcce2
Gul Jamal Zim <guljamalzim@gmail.com>
2022-03-01 11:05:53 +0600

    Flesh out Comment model & factory classes

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'post_id',
+        'user_id',
+        'content',
+    ];
+
+    public function author()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function post()
+    {
+        return $this->belongsTo(Post::class);
+    }
 }
--- database/factories/CommentFactory.php
+++ database/factories/CommentFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class CommentFactory extends Factory
@@ -14,7 +16,9 @@ class CommentFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'user_id' => User::inRandomOrder()->first(),
+            'post_id' => Post::inRandomOrder()->first(),
+            'content' => $this->faker->paragraph(),
         ];
     }
 }
