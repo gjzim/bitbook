d770208b343af747304daa50233a2f9da0ae4c94
Gul Jamal Zim <guljamalzim@gmail.com>
2022-03-04 10:54:39 +0600

    Creat Notification model & table migration

---
+++ database/migrations/2022_03_02_054922_create_notifications_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNotificationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notifications', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->enum('type', ['action', 'announcement', 'misc'])->default('action');
+            $table->boolean('checked')->index()->default(false);
+            $table->text('text')->nullable();
+            $table->text('url')->nullable();
+            $table->text('image_url')->nullable();
+            $table->timestamp('checked_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notifications');
+    }
+}
