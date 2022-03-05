3a76fed005816cbce4c3e8736538cc51ba1dd9b5
Gul Jamal Zim <guljamalzim@gmail.com>
2022-03-05 09:24:41 +0600

    Make notification count bubble dynamic using alpine

--- resources/views/components/notification-count-bubble.blade.php
+++ resources/views/components/notification-count-bubble.blade.php
@@ -1,6 +1,7 @@
-<span
+<span x-data="{
+    count: '{{ $count }}'
+}" x-show="count > 0" x-text="count"
     {{ $attributes->merge([
-        'class' => 'inline-block h-5 ml-1 px-2 leading-5 font-mono font-bold text-xs bg-red-500 text-white rounded-2xl text-shadow',
+        'class' => 'inline-block h-5 ml-2 px-2 leading-5 font-mono font-bold text-xs bg-red-500 text-white rounded-2xl text-shadow',
     ]) }}>
-    {{ $count }}
 </span>
