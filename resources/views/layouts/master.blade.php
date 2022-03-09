181d0f27c9a93e6c25f4c7d223adc464eb88f2ea
Gul Jamal Zim <guljamalzim@gmail.com>
2022-03-09 12:26:56 +0600

    Add empty script tag to resolve FOUC issue

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -15,6 +15,11 @@
 
     <!-- Styles -->
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+    <script>
+        /*to prevent Firefox FOUC, this must be here*/
+        let FF_FOUC_FIX;
+    </script>
 </head>
 
 <body class="font-sans antialiased text-gray-800">
