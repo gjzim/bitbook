afd44f145ce0b6d517db782683285c155516ea8d
Gul Jamal Zim <guljamalzim@gmail.com>
2022-03-13 10:10:20 +0600

    Restructure master and guest layout file to extend app layout

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,29 +1,6 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('layouts.app')
 
-<head>
-    <meta charset="utf-8">
-    <link rel="icon" href="/favicon.png" />
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name') }}</title>
-
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
-
-    <!-- Styles -->
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-
-    <script>
-        /*to prevent Firefox FOUC, this must be here*/
-        let FF_FOUC_FIX;
-    </script>
-</head>
-
-<body class="font-sans antialiased text-gray-800">
+@section('body')
     <div class="relative container flex flex-col min-h-screen mx-auto">
         @include('layouts.partials.header')
 
@@ -43,9 +20,4 @@
     </div>
 
     @yield('overlays')
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-</body>
-
-</html>
+@endsection
