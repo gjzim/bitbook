fe8bda59793302378cc3b35a152269aadc77e402
Gul Jamal Zim <guljamalzim@gmail.com>
2022-04-07 14:10:46 +0600

    Increase vertical spacing of single posts

--- resources/views/posts/single/index.blade.php
+++ resources/views/posts/single/index.blade.php
@@ -1,4 +1,4 @@
-<div x-data="singlePostActions(post)" class="my-8">
+<div x-data="singlePostActions(post)" class="my-10">
     @include('posts.partials.header')
 
     @include('posts.partials.body')
