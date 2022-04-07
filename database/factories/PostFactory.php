c6979a488353067f2b5570ce2bb651a56f191587
Gul Jamal Zim <guljamalzim@gmail.com>
2022-04-07 14:16:47 +0600

    Set up postSeeder

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -2,8 +2,10 @@
 
 namespace Database\Factories;
 
+use App\Models\Post;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 class PostFactory extends Factory
 {
@@ -18,5 +20,20 @@ class PostFactory extends Factory
             'content' => $this->faker->paragraph(),
             'user_id' => User::inRandomOrder()->first()->id,
         ];
+    }
+
+    /**
+     * Configure the model factory.
+     *
+     * @return $this
+     */
+    public function configure()
+    {
+        return $this->afterCreating(function (Post $post) {
+            if (random_int(1, 15) > 13) {
+                $imageUrl = $this->faker->imageUrl(900, 550, Str::random(5), true);
+                $post->addMediaFromUrl($imageUrl)->toMediaCollection('images');
+            }
+        });
     }
 }
